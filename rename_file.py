466f80e6a659ba05ddbb3459f07be6d568d6d6c9
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-11 21:57:50 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -11,9 +11,10 @@ def pig_latin(text):
     # Turn the list back into a phrase
   return ___
 		
-print(pig_latin("hello how are you")) 
+print(pig_latin("hello how are you"))
 # Should be "ellohay owhay reaay ouyay"
-print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+print(pig_latin("programming in python is fun"))
+# Should be "rogrammingpay niay ythonpay siay unfay"
 
 
 
