fee64fefe20e30e9a65c9013e713bdc998bdf541
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-11 23:08:19 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -14,8 +14,8 @@ def pig_latin(text):
         temp_word = word[1:] + word[0] + "ay"
         # print(temp_word)
         # Turn the list back into a phrase
-        say += temp_word
         temp_list.append(say)
+        # say += temp_word
         # say = ' '.join(temp_list)
     print(temp_list)
     return          # added words
