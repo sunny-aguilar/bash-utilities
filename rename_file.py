74a2054db425cd793118dc1c0d7c806b26625e74
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-11 20:43:43 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -3,19 +3,15 @@
 # the form (old_name, new_name).
 
 # That is, given the following list of filenames
-
 # filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
-
 # complete the starter code to generate the following newfilenames list of tuples
-
 # newfilenames = [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'),
 # ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('math.hpp', 'math.h'), ('hpp.out', 'hpp.out')]
 
-
-
-
 filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
 newfilenames = []
+
+
 
 print (newfilenames)
 # Should be [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'), ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('math.hpp', 'math.h'), ('hpp.out', 'hpp.out')]
