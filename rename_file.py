96ceef86bc5db902838e8d4f23615376c107598c
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-12 21:11:34 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -4,10 +4,6 @@
 #The email_list function receives a dictionary, which contains domain
 # names as keys, and a list of users as values. Fill in the blanks to
 # generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
-
-
-
-
 def email_list(domains):
 	emails = []
     for ___:
