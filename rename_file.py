c740589935084b36d4584b75cbb88cb6ddee6b01
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-12 21:42:49 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -5,12 +5,11 @@
 # names as keys, and a list of users as values. Fill in the blanks to
 # generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
 def email_list(domains):
-    print(domains.items()
     emails = []
+    print(domains.items()
     for names, users in domains.items():
         for user in users:
             emails.append(user+"@"+names)
-        #     emails.___
     return(emails)
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
