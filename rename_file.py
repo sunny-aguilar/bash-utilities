54f3ed281a11d6223000a6ee3414635d67a224c2
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-12 22:06:33 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -34,7 +34,7 @@ def email_list(domains):
             emails.append(user+"@"+names)
     return(emails)
 
-print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+# print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
 
 
 
