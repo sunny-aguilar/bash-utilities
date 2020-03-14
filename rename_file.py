43f7256c21b5a898fb0b7f2bf3f59a5a7c35cb3c
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:50:20 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -2,6 +2,13 @@
 
 
 
+def highlight_word(sentence, word):
+	return(sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
 
 
 
@@ -27,23 +34,14 @@ def format_address(address_string):
     # Return the formatted string
     return "house number {} on street named {}".format(house_number, street_name)
 
-print(format_address("123 Main Street"))
+# print(format_address("123 Main Street"))
 # Should print: "house number 123 on street named Main Street"
 
-print(format_address("1001 1st Ave"))
+# print(format_address("1001 1st Ave"))
 # Should print: "house number 1001 on street named 1st Ave"
 
-print(format_address("55 North Center Drive"))
+# print(format_address("55 North Center Drive"))
 # Should print "house number 55 on street named North Center Drive"
-
-
-
-
-
-
-
-
-
 
 
 
