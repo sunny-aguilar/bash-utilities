89858b43c33c12d1815f90c9b17cf46a0fa7ad02
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 18:05:27 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -4,7 +4,13 @@
 
 
 
+def car_listing(car_prices):
+  result = ""
+  for key in car_prices:
+    result += "{} costs {} dollars".format(key, car_prices[key]) + "\n"
+  return result
 
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
 
 
 
