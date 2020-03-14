4bad6cb877b9a06b7280c91610794c1d6d1e67f0
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:43:40 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -1,5 +1,30 @@
 # Author:           Sandro Aguilar
 
+def format_address(address_string):
+    # Declare variables
+    house_number = ''
+    stree_name = ''
+    # Separate the address string into parts
+
+    # Traverse through the address parts
+    for __:
+    # Determine if the address part is the
+    # house number or part of the street name
+
+    # Does anything else need to be done
+    # before returning the result?
+
+    # Return the formatted string
+    return "house number {} on street named {}".format(__)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
 
 
 
