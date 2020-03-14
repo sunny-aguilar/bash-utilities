1b9d2cd261b42a8ef4f58da50009800b979772d7
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:38:58 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -1,5 +1,20 @@
 # Author:           Sandro Aguilar
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #The groups_per_user function receives a dictionary, which contains group names
 # with the list of users. Users can belong to multiple groups. Fill in the blanks
 # to return a dictionary with the users as keys and a list of their groups as values.
