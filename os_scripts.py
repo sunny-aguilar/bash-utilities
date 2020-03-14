0b17aafeb6cb51abe65581c86c22f4babad3ea29
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-14 15:47:05 -0700

    bash

--- os_scripts.py
+++ os_scripts.py
@@ -9,13 +9,11 @@ usage = 0
 
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
-    free = du.free / du.total * 100
-    txt1 = "Free disk space is {}".format(free)
+    global free = du.free / du.total * 100
     return free > 20
 
 def check_cpu_usage():
-    usage = psutil.cpu_percent(1)
-    txt2 = "CPU usage is {}".format(usage)
+    global usage = psutil.cpu_percent(1)
     return usage < 75
 
 
