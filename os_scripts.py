6fcc9fbaed25e494412099d33aab2a5d518d6d14
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-14 15:44:04 -0700

    bash

--- os_scripts.py
+++ os_scripts.py
@@ -6,17 +6,19 @@ import psutil
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
-    txt = "Free disk space is {}".format(free)
-    print(txt)
+    txt1 = "Free disk space is {}".format(free)
     return free > 20
 
 def check_cpu_usage():
     usage = psutil.cpu_percent(1)
-    txt = "CPU usage is {}".format(usage)
+    txt2 = "CPU usage is {}".format(usage)
     print(txt)
     return usage < 75
 
-if not check_disk_usage("/") and not check_cpu_usage:
+
+if not check_disk_usage("/") or not check_cpu_usage:
     print("ERROR!")
 else:
     print("Everything is ok!")
+    print(txt1)
+    print(txt2)
