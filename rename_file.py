36e2b0c50d365e27056e640f8fcdcfb23c0051c5
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 23:10:20 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -31,7 +31,14 @@ def generate_report(machines):
             user_list = ", ".join(users)
             print("{}: {}".format(machine, user_list))
 
-
+events = [
+    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+    Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+    Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
+]
 
 
 
