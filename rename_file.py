1e2faa488ebb525da49b21c59e4b735498faa493
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:50:24 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -47,7 +47,6 @@ def format_address(address_string):
 
 
 
-
 #The groups_per_user function receives a dictionary, which contains group names
 # with the list of users. Users can belong to multiple groups. Fill in the blanks
 # to return a dictionary with the users as keys and a list of their groups as values.
