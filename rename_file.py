12377133a1cbe320c005db5a96b650f7cf0e48c2
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 23:08:55 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -4,7 +4,7 @@ class Event:
     def __init__(self, event_date, event_type, machine_name, user):
         self.date = event_date
         self.type = event_type
-    self.machine = machine_name
+        self.machine = machine_name
     self.user = user
 
 def get_event_date(event):
