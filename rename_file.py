1a3f1c5a0be0f88f10c90c3ff30a12fa904f35fa
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:56:07 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -2,15 +2,19 @@
 
 
 
+
+animal = "Hippopotamus"
+
+
+
+
 def highlight_word(sentence, word):
 	return(sentence.replace(word,word.upper()))
 
-print(highlight_word("Have a nice day", "nice"))
-print(highlight_word("Shhh, don't be so loud!", "loud"))
-print(highlight_word("Automating with Python is fun", "fun"))
+# print(highlight_word("Have a nice day", "nice"))
+# print(highlight_word("Shhh, don't be so loud!", "loud"))
 
-
-
+# print(highlight_word("Automating with Python is fun", "fun"))
 
 
 def format_address(address_string):
