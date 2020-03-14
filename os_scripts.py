0e8c0961c54071daaabfc68ca25c221e67a8e4f0
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-14 14:42:55 -0700

    bash

---
+++ os_scripts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1):
+    return usage < 75
+
