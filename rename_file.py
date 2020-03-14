ed328afb8c3678552dc99c7b080628ed42a49841
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-13 17:57:28 -0700

    bash

--- rename_file.py
+++ rename_file.py
@@ -3,6 +3,11 @@
 
 
 
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+host_addresses.keys()
+
+
+
 colors = ["red", "white", "blue"]
 colors.insert(2, "yellow")
 print(colors)
