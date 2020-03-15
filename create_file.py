34c0c923f81e91313104165f08f61b032eb6707c
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-15 13:19:18 -0700

    bash

--- create_file.py
+++ create_file.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 import getopt           # parse command-line args
 import sys              # access command line args
 import os
 
-def create_python_script(filename):
+#def create_python_script(filename):
 
 
-
+print('Num of args:', len(sys.argv), ' arguments.')
