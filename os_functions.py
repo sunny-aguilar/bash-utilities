8c1497c2f1e374f45e950388a5f93b6970526f20
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-15 12:28:59 -0700

    bash

--- os_functions.py
+++ os_functions.py
@@ -26,5 +26,6 @@ for name in os.listdir(dir):
     fullname = os.path.isdir(dir, name)
     if os.path.isdir(fullname):
         print('{} is a directory'.format(fullname))
-
+    else:
+        print('{} is a file'.format(fullname))
 
