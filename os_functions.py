90dd4a3c96cc01c7aa64e823ed834b90eb845be4
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-15 11:39:49 -0700

    bash

--- os_functions.py
+++ os_functions.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import os
+import datetime
 
 txt = "Current working dir: {}"
 print(txt.format(os.getcwd()))
 
 txt = "File size: {}"
 print(txt.format(os.path.getsize('text_file')))
+
+
