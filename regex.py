8fb7a1077c23d98fd25c213b417c8eb612853420
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-16 19:29:52 -0700

    bash

--- regex.py
+++ regex.py
@@ -3,9 +3,7 @@
 # Author:              Sandro Aguilar
 # Date:                Month day, 2020
 # Class:               Coursera Regex
-# Description:         Introl to regular expressions in Python
-#                      
-#                      
+# Description:         Intro to regular expressions in Python
 #                      
 ####################################################################
 
