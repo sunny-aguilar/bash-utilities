5b29e85a2355427aa8ad650ede6f25af749a8c9a
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-17 11:35:01 -0700

    bash

--- regex.py
+++ regex.py
@@ -88,8 +88,10 @@ print(result[1])
 print(result[2])
 print("{} {}".format(result[2], result[1]))
 
-
-
-
+def rearrange_name(name):
+    result = re.search(r'^(\w*), (\w*)$', name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
 
 
