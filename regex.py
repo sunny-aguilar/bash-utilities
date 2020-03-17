b1d428bc3af67d3f3f0f0ab30e4b79e41988f183
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-17 11:37:29 -0700

    bash

--- regex.py
+++ regex.py
@@ -92,7 +92,6 @@ def rearrange_name(name):
     result = re.search(r'^(\w*), (\w*)$', name)
     if result is None:
         return name
-    print('reached')
     return "{} {}".format(result[2], result[1])
 
-rearrange_name('Lovelace, Ada')
+print(rearrange_name('Lovelace, Ada'))
