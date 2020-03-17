1b15b267a766c9f67e877a78170f99aebe70beb1
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-17 11:57:48 -0700

    bash

--- regex.py
+++ regex.py
@@ -101,7 +101,7 @@ print(rearrange_name('Ritchie, Dennis'))
 print(re.search(r'[a-zA-Z]{5}', 'a ghost'))
 print(re.search(r'[a-zA-Z]{5}', 'a scary ghost appeared'))
 print(re.findall(r'\b[a-zA-Z]{5}\b', 'a scary ghost appeared')) # find complete words \b
-
+print(re.findall(r'\w{5-10}', 'I really like strawberries'))
 
 
 
