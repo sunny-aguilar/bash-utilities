33044d825319af740bf7df83e50d9a4f8be0499e
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-17 12:20:09 -0700

    bash

--- regex.py
+++ regex.py
@@ -101,13 +101,12 @@ print(rearrange_name('Ritchie, Dennis'))
 print(re.search(r'[a-zA-Z]{5}', 'a ghost'))
 print(re.search(r'[a-zA-Z]{5}', 'a scary ghost appeared'))
 print(re.findall(r'\b[a-zA-Z]{5}\b', 'a scary ghost appeared')) # find complete words \b
-print(re.findall(r'\w{5-10}', 'I really like strawberries'))
+print(re.findall(r'\w{5,10}', 'I really like strawberries'))
 
 
 
 
 
-  
 
 
 
