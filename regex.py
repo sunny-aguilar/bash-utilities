79f2085a7cef59e3ea2d7a5e53c7166954e65f14
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-16 21:40:01 -0700

    bash

--- regex.py
+++ regex.py
@@ -37,3 +37,8 @@ print(re.search(r'p.ng', 'penguin'))
 print(re.search(r'p.ng', 'clapping'))
 print(re.search(r'p.ng', 'sponge'))
 
+# case insensitive
+print(r'p.ng', 'Pangaea', re.IGNORECASE)
+
+
+
