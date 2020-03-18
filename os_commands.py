74434dcf8ccc37a07edb068c2a507e7b36fad1c0
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-18 16:05:36 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -2,8 +2,8 @@
 ####################################################################
 # Author:               Sandro Aguilar
 # Date:                 Month day, 2020
-# Class:                CS XXX
-# Description:          Enter description here
+# Class:                Coursera: Using Python to interact with the OS
+# Description:          Managing data and processes.
 #                       
 #                       
 #                       
