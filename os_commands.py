7bc54af933abea444f9bfe9b5b2fef56a7719033
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 16:23:44 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -4,13 +4,11 @@
 # Date:                 Month day, 2020
 # Class:                Coursera: Using Python to interact with the OS
 # Description:          Managing data and processes.
-#                       
-#                       
 ####################################################################
 
 # input function to prompt for user input
 name = input('Please enter your name: ')
-
+print("Hello, " + name)
 
 
 
