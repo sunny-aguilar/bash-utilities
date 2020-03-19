1018d47d154954d277c4546f97b939a9e84d4102
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 16:37:39 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -19,5 +19,6 @@ while (cont.lower() == 'y'):
     hours = int(input('Enter the number of hours: '))
     minutes = int(input('Enter the number of minutes: '))
     seconds = int(input('Enter the number of seconds: '))
-
-
+    print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+    print()
+    cont = 
