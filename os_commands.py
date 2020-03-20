02a295820960947a8d9c97500749e85e950bda14
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 17:46:48 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -15,14 +15,25 @@ def to_seconds(hours, minutes, seconds):
     return hours*3600 + minutes * 60 + seconds
 
 cont = 'y'
-while (cont.lower() == 'y'):
-    hours = int(input('Enter the number of hours: '))
-    minutes = int(input('Enter the number of minutes: '))
-    seconds = int(input('Enter the number of seconds: '))
-    print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
-    print()
-    cont = input('do you want to do another conversion? [y to continue] ')
+# while (cont.lower() == 'y'):
+#     hours = int(input('Enter the number of hours: '))
+#     minutes = int(input('Enter the number of minutes: '))
+#     seconds = int(input('Enter the number of seconds: '))
+#     print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+#     print()
+#     cont = input('do you want to do another conversion? [y to continue] ')
 
 print('Good bye!')
+
+
+# input streams
+data = input("This will come from STDIN: ")
+print("We we write it to STDOUT: " + data + 1)
+print("Now we generate an error to STDERR: " + data + 1)
+
+
+
+
+
 
 
