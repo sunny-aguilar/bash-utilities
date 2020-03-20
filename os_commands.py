b74b27a84c1c77db766cbaa08be9a818e624f0af
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 21:39:39 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -27,9 +27,9 @@ cont = 'y'
 
 
 # input streams
-data = input("This will come from STDIN: ")
-print("We we write it to STDOUT: " + data)
-print("Now we generate an error to STDERR: " + data + 1)
+# data = input("This will come from STDIN: ")
+# print("We we write it to STDOUT: " + data)
+# print("Now we generate an error to STDERR: " + data + 1)
 
 
 # environment variables
