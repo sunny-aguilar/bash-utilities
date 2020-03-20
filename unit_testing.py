6a1dde0bd0029f923ef0d66e433b94f4cb02c608
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-20 14:03:46 -0700

    bash

--- unit_testing.py
+++ unit_testing.py
@@ -11,7 +11,6 @@ def rearrange_name(name):
     result = re.search(r'^([\w .]*), ([\w .]*)$', name)
     if result is None:
         return name
-    if result
     return '{} {}'.format(result[2], result[1])
 
 
