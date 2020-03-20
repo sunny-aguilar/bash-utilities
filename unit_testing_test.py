90ba3ded565110343537a3840282295f4ca8787f
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-20 13:22:26 -0700

    bash

--- unit_testing_test.py
+++ unit_testing_test.py
@@ -10,8 +10,9 @@ from rearrange import rearrange_name
 
 class TestRearrange(unittest.TestCase):
     def test_basic(self):
-
-
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
 
 
 
