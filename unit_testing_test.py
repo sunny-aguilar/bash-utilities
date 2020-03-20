fb873a925e84511b8bb5e2ecd1fc04dfcdc1f621
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-20 13:29:09 -0700

    bash

--- unit_testing_test.py
+++ unit_testing_test.py
@@ -6,13 +6,22 @@
 # Description:          Enter description here
 ####################################################################
 import unittest
-from rearrange import rearrange_name
+from unit_testing import rearrange_name
 
 class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelace, Ada"
         expected = "Ada Lovelace"
         self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+
+
+
 
 unittest.main()
 
