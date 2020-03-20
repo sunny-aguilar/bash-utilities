749e91fb740d6bf2ac6c982595cc9d5d9b193499
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 22:59:27 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -73,8 +73,8 @@ result = subprocess.run(['host', '8.8.8.8'], capture_output=True)
 print(result)
 print(result.stdout.decode().split())
 
-
-
+result = subprocess.run(['rm', 'does_not_exist'], capture_output=True)
+print(result.returncode)
 
 
 
