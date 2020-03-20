8647ed86839cca7ad5ee9d5dd6304a9a758ec5b1
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-19 23:37:10 -0700

    bash

--- os_commands.py
+++ os_commands.py
@@ -91,8 +91,11 @@ cont = 'y'
 # filtering logs files with regular expressions
 import re
 
+pattern1 = r'Sun'
 pattern = r"USER \((\w+)\)$"
+line1 = 'Sunny'
 line = 'Jul 6 14:04:02 computer.name CRON[29440]: USER (naughty user)'
+result = re.search(pattern1, line1)
 result = re.search(pattern, line)
 print(result)
 
