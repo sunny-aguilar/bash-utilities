566a29373348bb24930d76efc9c5bbb8a4415ee2
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-21 10:26:54 -0700

    bash

--- final.py
+++ final.py
@@ -12,8 +12,8 @@ regex = r"ticky: INFO: ([\w ]*) "
 line = "May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)"
 print(re.search(r"ticky: INFO: ([\w ]*) ", line))
 
-
-
+line = "May 27 11:45:40 ubuntu.local ticky: ERROR: Error creating ticket [#1234] (username)"
+print(re.search(r"ticky: ERROR: ([\w ]*) ", line))
 
 
 
