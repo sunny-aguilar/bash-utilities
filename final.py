61207a0d780e706b7d8210a40d70f0fba8409975
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-21 10:28:14 -0700

    bash

--- final.py
+++ final.py
@@ -16,8 +16,8 @@ line = "May 27 11:45:40 ubuntu.local ticky: ERROR: Error creating ticket [#1234]
 print(re.search(r"ticky: ERROR: ([\w ]*) ", line))
 
 
-
-
+fruit = {"oranges": 3, "apples": 5, "bananas": 7, "pears": 2}
+sorted(fruit.items())
 
 
 
