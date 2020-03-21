5afee810bd7fe94ce613b75b22bef803399e7c97
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-21 11:24:54 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -18,7 +18,7 @@ with open('sys.log') as file:
         #print(parsed_row)
         regex_info = r"INFO"
         regex_error = r"ERROR"
-        results = re.search(regex_info, parsed_row)
+        results = re.search(regex_error, parsed_row)
         if results is not None:
             print(results)
             print(results[0])
