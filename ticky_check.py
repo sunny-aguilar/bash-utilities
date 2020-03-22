015423cdf4372277d39b1362f9e0eb2fcbbd374d
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 14:43:05 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -48,7 +48,7 @@ with open('sys.log') as file:
 
 
         print(results_user[0])
-        print(regex_TYPE)
+        print(results_TYPE)
 
         # if username is found, parse message
         # if results_user is not None:
