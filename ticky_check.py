a04be15ebff8fbf497adca4f0d9063c0cf33b8bf
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 12:44:08 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -17,9 +17,7 @@ with open('sys.log') as file:
         parsed_row = row.strip()
         #print(parsed_row)
         regex_error = r"ERROR(\s)*([\w ]*)"
-        
         results_error = re.search(regex_error, parsed_row)
-        
 
         if results_error is not None:
             #print(results)
@@ -31,7 +29,7 @@ with open('sys.log') as file:
                 error_message[results_error[0]] += 1
 
         regex_user = r"\([\w.]*\)$"
-        regex_user = r"\([\w.]*\)$"regex_info = r"INFO(\s)*([\w ]*)"
+        regex_info = r"INFO(\s)*([\w ]*)"
         regex_message = r'[A-Z]{4}[A-Z]*(\s)*([\w ]*)'
 
         user_results = re.search(regex_user, parsed_row)
