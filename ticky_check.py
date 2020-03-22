081ae0827eb4cfc871342172cdf7936548152d8e
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 15:04:47 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -52,13 +52,13 @@ with open('sys.log') as file:
 
         if results_TYPE[0] == "INFO ":
             if results_user is not None:
-                print(results_user[0] + " : " + print(results_TYPE[0]))
+                print(results_user[0] + " : " + results_TYPE[0]))
             # if user_count[results_user[0]] not in user_count:
             #     print(results_user[0])
 
         elif results_TYPE[0] == "ERROR":
             if results_user is not None:
-                print(results_user[0] + " : " + print(results_TYPE[0]))
+                print(results_user[0] + " : " + results_TYPE[0]))
 
 
 
