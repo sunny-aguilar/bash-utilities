230c8db14371041268774d61a6453befaabbacfc
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 13:55:14 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -55,7 +55,7 @@ with open('sys.log') as file:
                 if results_info is not None:
                     print("INFO:")
                     print(results_info[0])
-                    if results_info[0] no in user_count:
+                    if results_info[0] not in user_count:
                         user_count[results_info[0]] = 0 + 1
                     else:
                         user_count[regex_info[0]] = += 1
