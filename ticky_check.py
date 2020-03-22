e274df5b80423285554019a5d84efa4d6633dd79
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 15:24:10 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -46,7 +46,7 @@ with open('sys.log') as file:
         #regex_user = r"\([\w.]*\)$"
         #results_user = re.search(regex_user, parsed_row)
         regex_user = r"\((.*?)\)"
-        results_user = re.search(regex_user, parsed_row)
+        results_user = re.search(regex_user, parsed_row).group(1)
 
 
         #print(results_user[0])
@@ -55,7 +55,7 @@ with open('sys.log') as file:
         if results_TYPE[0] == "INFO ":
             if results_user is not None:
                 print(results_user[0] + " : " + results_TYPE[0])
-
+                
                 # if user_count[results_user[0]] not in user_count:
                 #     user_count[results_user[0]] = 0
 
