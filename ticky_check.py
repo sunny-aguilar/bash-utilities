54827c01cb2269d2f7875e3298c660507e205237
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-03-22 12:43:32 -0700

    bash

--- ticky_check.py
+++ ticky_check.py
@@ -16,21 +16,26 @@ with open('sys.log') as file:
     for row in file:
         parsed_row = row.strip()
         #print(parsed_row)
-        regex_info = r"INFO(\s)*([\w ]*)"
-        info_results = re.search(regex_info, parsed_row)
+        regex_error = r"ERROR(\s)*([\w ]*)"
+        
+        results_error = re.search(regex_error, parsed_row)
+        
 
-        if info_results is not None:
+        if results_error is not None:
             #print(results)
-            print(info_results[0])
+            print(results_error[0])
             #print(results[1])
-            if info_results[0] not in error_message:
-                error_message[info_results[0]] = 0 + 1
+            if results_error[0] not in error_message:
+                error_message[results_error[0]] = 0 + 1
             else:
-                error_message[info_results[0]] += 1
+                error_message[results_error[0]] += 1
 
         regex_user = r"\([\w.]*\)$"
-        regex_user = r"\([\w.]*\)$"
+        regex_user = r"\([\w.]*\)$"regex_info = r"INFO(\s)*([\w ]*)"
+        regex_message = r'[A-Z]{4}[A-Z]*(\s)*([\w ]*)'
+
         user_results = re.search(regex_user, parsed_row)
+        results_info = re.search(regex_info, parsed_row)
         if user_results is not None:
             print(user_results[0])
             if user_results[0] not in user_count:
